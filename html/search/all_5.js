1ba0a1a2a837ada642bcbdafcef40e2b646742c7
Travis CI <travis@travis-ci.org>
2018-07-08 00:22:39 +0000

    Deploy code docs to GitHub Pages Travis build: 13
    Commit: 4a1f7275af9fe96d089176b970edb0eb13bce1fa

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['serialnumber',['serialnumber',['../class_adafruit___s_g_p30.html#a3380958b75cc73b1e01f1ac66c47f4b8',1,'Adafruit_SGP30']]],
+  ['sethumidity',['setHumidity',['../class_adafruit___s_g_p30.html#a3cea979c8b14138cef092f13102b0e22',1,'Adafruit_SGP30']]],
   ['setiaqbaseline',['setIAQBaseline',['../class_adafruit___s_g_p30.html#ad6edd8767e42f0862c7a0ae68d95286c',1,'Adafruit_SGP30']]],
   ['sgp30_5fcrc8_5finit',['SGP30_CRC8_INIT',['../_adafruit___s_g_p30_8h.html#ad0265d35c765ef5deb76d71aa8b335a8',1,'Adafruit_SGP30.h']]],
   ['sgp30_5fcrc8_5fpolynomial',['SGP30_CRC8_POLYNOMIAL',['../_adafruit___s_g_p30_8h.html#a06547dd2635c9174f1570a57774726be',1,'Adafruit_SGP30.h']]],
--- html/search/functions_4.js
+++ html/search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['sethumidity',['setHumidity',['../class_adafruit___s_g_p30.html#a3cea979c8b14138cef092f13102b0e22',1,'Adafruit_SGP30']]],
   ['setiaqbaseline',['setIAQBaseline',['../class_adafruit___s_g_p30.html#ad6edd8767e42f0862c7a0ae68d95286c',1,'Adafruit_SGP30']]]
 ];
