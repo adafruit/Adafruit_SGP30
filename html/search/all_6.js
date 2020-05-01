efbaa6a0edfbc0735d1234d7f61fa1da7c84790c
Doxygen CI <travis@travis-ci.org>
2020-05-01 17:57:00 +0000

    Deploy code docs to GitHub Pages Travis build: 29
    Commit: 48ac1f49580a32e4024d17a03eeee827ccf3f1de

--- html/search/all_6.js
+++ html/search/all_6.js
@@ -7,5 +7,6 @@ var searchData=
   ['sgp30_5fcrc8_5fpolynomial',['SGP30_CRC8_POLYNOMIAL',['../_adafruit___s_g_p30_8h.html#a06547dd2635c9174f1570a57774726be',1,'Adafruit_SGP30.h']]],
   ['sgp30_5ffeatureset',['SGP30_FEATURESET',['../_adafruit___s_g_p30_8h.html#a71750860804e40db8fac33ac6484a157',1,'Adafruit_SGP30.h']]],
   ['sgp30_5fi2caddr_5fdefault',['SGP30_I2CADDR_DEFAULT',['../_adafruit___s_g_p30_8h.html#a06002cbb43d632a5f0007e0af6e51993',1,'Adafruit_SGP30.h']]],
-  ['sgp30_5fword_5flen',['SGP30_WORD_LEN',['../_adafruit___s_g_p30_8h.html#a5f79da0969f64a6d76eaa07322e4662a',1,'Adafruit_SGP30.h']]]
+  ['sgp30_5fword_5flen',['SGP30_WORD_LEN',['../_adafruit___s_g_p30_8h.html#a5f79da0969f64a6d76eaa07322e4662a',1,'Adafruit_SGP30.h']]],
+  ['softreset',['softReset',['../class_adafruit___s_g_p30.html#a25e5f561a3b67c44f5d9c77fd6c8d3e1',1,'Adafruit_SGP30']]]
 ];
--- html/search/functions_4.js
+++ html/search/functions_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['sethumidity',['setHumidity',['../class_adafruit___s_g_p30.html#a3cea979c8b14138cef092f13102b0e22',1,'Adafruit_SGP30']]],
-  ['setiaqbaseline',['setIAQBaseline',['../class_adafruit___s_g_p30.html#ad6edd8767e42f0862c7a0ae68d95286c',1,'Adafruit_SGP30']]]
+  ['setiaqbaseline',['setIAQBaseline',['../class_adafruit___s_g_p30.html#ad6edd8767e42f0862c7a0ae68d95286c',1,'Adafruit_SGP30']]],
+  ['softreset',['softReset',['../class_adafruit___s_g_p30.html#a25e5f561a3b67c44f5d9c77fd6c8d3e1',1,'Adafruit_SGP30']]]
 ];
